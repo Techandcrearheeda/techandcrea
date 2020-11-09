311b265feb9c01b49d518245e993289fd1e8813a
vanjasoa <vanjablou@gmail.com>
2020-11-09 08:56:52 +0300

    feat(landing): init

---
+++ components/home2/ClientsTwo.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="commonSection client_2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h4 class="sub_title">our offices</h4>
+          <h2 class="sec_title">offices near you</h2>
+          <p class="sec_desc">
+            We are committed to providing our customers with exceptional service while<br> offering our employees the best training.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-sm-6 col-md-3">
+          <div class="singleClient_2">
+            <h3>Ambohimanala</h3>
+            <p>Analamanga Antananarivo/p>
+            <p>needhelp@Techandcrea.com</p>
+            <p>666 888 000</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-md-3">
+          <div class="singleClient_2">
+            <h3>Ambohimanala</h3>
+            <p>Analamanga Antananarivo</p>
+            <p>needhelp@Techandcrea.com</p>
+            <p>666 888 000</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-md-3">
+          <div class="singleClient_2">
+            <h3>Ambohimanala</h3>
+            <p>Analamanga Antananarivo</p>
+            <p>needhelp@Techandcrea.com</p>
+            <p>666 888 000</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 col-md-3">
+          <div class="singleClient_2">
+            <h3>Ambohimanala</h3>
+            <p>Analamanga Antananarivo</p>
+            <p>needhelp@Techandcrea.com</p>
+            <p>666 888 000</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+      name: "ClientsTwo"
+    }
+</script>
+
+<style scoped>
+
+</style>
