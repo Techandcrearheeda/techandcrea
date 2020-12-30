38a555ef897b4b66c35289edd1855681092eada2
Techandcrearheeda <76274700+Techandcrearheeda@users.noreply.github.com>
2020-12-30 15:07:18 +0100

    modif mentions légales

---
+++ components/home/Mention.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="commonSection ab_agency">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-sm-6 col-md-6 PR_79">
+          <h4 class="sub_title">{{MentionContent.sub_title}}</h4>
+          <h2 class="sec_title MB_45">{{MentionContent.sec_title}}</h2>
+          <p class="sec_desc">
+            {{MentionContent.sec_desc}}
+          </p>
+          <a class="common_btn red_bg" href="/mention
+          "><span>{{MentionContent.common_btn}}</span></a>
+        </div>
+        <div class="col-lg-6 col-sm-6 col-md-6">
+          <div class="ab_img1">
+            <img src="/images/home_1/2.jpg" alt=""/>
+          </div>
+          <div class="ab_img2">
+            <img src="/images/home_1/1.jpg" alt=""/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+      name: "Mention",
+      props:{
+        MentionContent:{
+          type: Object
+        }
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/mention.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <PageBanner pageTitle="Mentions légales" />
+    <Mention :MentionContent='MentionPage'/>
+
+    <!-- <ClientsTwo /> -->
+  </div>
+</template>
+
+<script>
+  import PageBanner from "~/components/pages/PageBanner";
+  import Mention from "~/components/pages/Mention";
+  import Map from "~/components/home2/Map";
+ import ClientsTwo from "~/components/home2/ClientsTwo";
+  export default {
+    components: {
+      Mention
+    },
+    head(){
+      return {
+        title: "Techandcrea | Mentions légales"
+      }
+    },
+    data() {
+        return {
+          MentionPage:{
+              sub_title: 'CONTACTEZ-NOUS',
+              sec_title: 'écrivez-nous un message',
+              sec_desc :['La raison d\'être d\'une entreprise est de créer et de garder un client '],
+              common_btn:'Envoyer le message'
+          },
+      }
+    }
+  }
+ 
+</script>
